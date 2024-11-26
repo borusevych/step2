78182b64e99d237d496d5ff2c5f4b89c0ff074af
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-11-26 19:14:02 +0200

    windows fix

--- java-web/src/main/java/web/ResourcesOps.java
+++ java-web/src/main/java/web/ResourcesOps.java
@@ -4,13 +4,16 @@ import java.net.URISyntaxException;
 
 public class ResourcesOps {
 
+  private static final boolean win = System.getProperty("os.name").toLowerCase().contains("win");
+
   public static String dirUnsafe(String prefix) {
     try {
-      return ResourcesOps.class
+      String path = ResourcesOps.class
         .getClassLoader()
         .getResource(prefix)
         .toURI()
         .getPath();
+      return win ? path.substring(1) : path;
     } catch (URISyntaxException e) {
       throw new RuntimeException(e);
     }
